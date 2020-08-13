3afa6ca717b4ac05618403d17a1656d7b83b6154
chuck francis <chuck_francis10@yahoo.com>
2020-08-13 13:33:01 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SpringFowardFallBack08132020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It's Sprin...!");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(i + ",");
+            }
+
+            Console.WriteLine("\nOh no, it's Fall...");
+            for (int i = 10;  0; i--) 
+            {
+                Console.Write(i + ", ");
+            }
+        }
+    }
+}
